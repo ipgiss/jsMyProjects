582f27f2dcf18c94651642a371b1da9074917bfe
ipgiss <104978667+ipgiss@users.noreply.github.com>
2023-02-08 00:04:28 +0300

    add files lesson 01

---
+++ 01_addBordered/01_readme.txt
@@ -0,0 +1,3 @@
+Добавьте изображениям, по которым кликнул пользователь, класс bordered в оформлении которого содержиться граница и тень.
+
+Add a user-clicked image with a bordered class that contains a border and shadow.
---
+++ 01_addBordered/script.js
@@ -0,0 +1,12 @@
+//
+//1. добавить событие click на элемент
+
+document.onclick = function (event) {
+  //   console.log(event.target.tagName);
+
+  if (event.target.tagName == "IMG") {
+    event.target.classList.add("bordered");
+  }
+};
+
+//target.classList.add("bordered");
